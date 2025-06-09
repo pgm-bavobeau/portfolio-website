926fb0effcbe9d6bbca02b37c41d892f87398832
Bavo Beaumon <bavobeau@student.arteveldehs.be>
2025-06-09 20:29:40 +0200

    Refactor index.html and project.html for improved structure and content; update styles.css for better layout and responsiveness

---
+++ js/projecten-loader.js
@@ -0,0 +1,19 @@
+fetch("../projects.json")
+  .then(res => res.json())
+  .then(projects => {
+    const container = document.getElementById("projects");
+    // Reverse the array and remove first item
+    const reversedProjects = projects.slice(1).reverse();
+    container.innerHTML = reversedProjects.map(p => `
+      <li class="card" data-aos="fade-up">
+      <a href="../project.html?id=${p.id}" class="card__link">
+      ${p.thumb ? `<img src="../${p.thumb}" alt="${p.title}" class="card__thumbnail">` : ''}
+        <h4 class="card__title">${p.title}</h4>
+        <div class="card__synopsis">${p.description}</div>
+        <div class="card__tags">
+        ${p.badges.map(b => `<div class="card__tag">${b}</div>`).join('')}
+        </div>
+      </a>
+      </li>
+    `).join('');
+  });
